16188e55ebbf2b694346e736a8161919b3edc9e3
Winston <user@MacBookPro.powerhub>
2025-04-24 12:37:58 +0100

    update assetprefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
   // produce a static “out” folder
   output: 'export',
   basePath: '/talo_public',
-  assetPrefix: '/talo_public/',
+  assetPrefix: 'https://winstonbarlowg.github.io/talo_public/',
 };
 
 export default nextConfig;
